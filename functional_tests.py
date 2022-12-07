6a99cd42e22ead435a738ccda27e973d7e04b86f
Vel <valera-shih@mail.ru>
2022-12-07 15:49:37 +0400

    'Расширен первый ФТ в комментариях, теперь использует unittest'

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,26 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """тест нового посетитея"""
+
+    def setUp(self) -> None:
+        """установка"""
+        self.bowser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        """демонтаж"""
+        self.bowser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """тест: можно начать список и получить его позже """
+        # домашняя страница
+        self.bowser.get('http://localhost:8000')
+        # заголовок и шапка страниц
+        self.assertIn('To-Do', self.bowser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
