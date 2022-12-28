298f464e48f79ead59cacc2bec01c49719c3dc77
Vel <valera-shih@mail.ru>
2022-12-28 13:49:51 +0400

    Добавлена запись новый списков и новый URL-адрес

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,6 @@ from lists.models import Item
 
 def home_page(request):
     """домашняя страница"""
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 
@@ -15,3 +12,9 @@ def view_list(request):
     """представление списка"""
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    """новый список"""
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
