c81834742dbdd34ff7af4aa025180bdc1f4c1c9d
Vel <valera-shih@mail.ru>
2023-01-01 14:49:13 +0400

    Новый URL + представления для добавления в существующие списки.ФТ проходит

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -96,7 +96,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Френсис получает уникальный URL-адрес
         francis_list_url = self.browser.current_url
-        self.assertRegex(francis_list_url, '/list/.+')
+        self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Опять таки, нет ни следа от списка Эдит
