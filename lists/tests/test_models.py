8de1b91a455090770e59aa30a74c2a1748785ded
Vel <valera-shih@mail.ru>
2023-02-08 20:24:16 +0400

    Разделены модульные тесты на два файла

---
+++ lists/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+
+class ListAndModelsTest(TestCase):
+    """Тест модели элемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        """Тест на сохранение и получения элементов списка"""
+        list_ = List()
+        list_.save()
+        fitst_item = Item()
+        fitst_item.text = 'The first (ever) list item'
+        fitst_item.list = list_
+        fitst_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.list = list_
+        second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+
+    def test_display_all_items(self):
+        """тест: отображаются все элементы списка"""
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -17,44 +17,6 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
 
-class ListAndModelsTest(TestCase):
-    """Тест модели элемента списка"""
-
-    def test_saving_and_retrieving_items(self):
-        """Тест на сохранение и получения элементов списка"""
-        list_ = List()
-        list_.save()
-        fitst_item = Item()
-        fitst_item.text = 'The first (ever) list item'
-        fitst_item.list = list_
-        fitst_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
-
-def test_display_all_items():
-    """тест: отображаются все элементы списка"""
-    list_ = List.objects.create()
-    Item.objects.create(text='itemey 1', list=list_)
-    Item.objects.create(text='itemey 2', list=list_)
-
-
 class ListViewTest(TestCase):
     """тест представления списка"""
 
