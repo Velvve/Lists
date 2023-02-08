05939038eb9b938e7555ebc5552007bc226f3919
Vel <valera-shih@mail.ru>
2023-02-08 17:27:03 +0400

    Добавлена проверка на отправку пустых строк

--- functional_tests/test_list_itemvalidation.py
+++ functional_tests/test_list_itemvalidation.py
@@ -10,4 +10,33 @@ class ItemValidationTest(FunctionalTest):
         """тест: нельзя добавлять пустые элементы списка"""
         # Эдит добавляет домашнюю страницу и случайно пытается
         # отправить пустой элемент списка. Она нажимает Enter
+        self.browser.get(self.live_server_url)
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+
+        # Домашняя страница обновляется, и появляется сообщение об ошибке
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element('css selector', '.has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        # Она пробует снова теперь с неким текстом для элемента и это работает
+        self.browser.find_element('id', 'id_new_item').send_keys('Buy milk')
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Попытка отправить второй пустой список
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+
+        # Эдит получает аналогичное предупреждение
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element('css selector', '.has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        # Заполнение новыми элементами
+        self.browser.find_element('id', 'id_new_item').send_keys('Make tea')
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: But milk')
+        self.wait_for_row_in_list_table('2: Make tea')
+
         self.fail('Напиши меня')
