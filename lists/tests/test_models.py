e9c6ae0b0cff9b47c879df6c6147177bfe3229f8
Vel <valera-shih@mail.ru>
2023-02-09 18:09:46 +0400

    Скорректировано новое представление списка с учетом валидации модели

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 
 class ListAndModelsTest(TestCase):
@@ -37,3 +38,13 @@ class ListAndModelsTest(TestCase):
         list_ = List.objects.create()
         Item.objects.create(text='itemey 1', list=list_)
         Item.objects.create(text='itemey 2', list=list_)
+
+    def test_cannot_save_empty_list_items(self):
+        """тест: нельзя добавить пустой элемент списка"""
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
+
+
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest
 from lists.views import home_page
 from django.template.loader import render_to_string
 from lists.models import Item, List
+from django.utils.html import escape
 
 
 class HomePageTest(TestCase):
@@ -65,6 +66,20 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         list_new = List.objects.first()
         self.assertRedirects(response, f'/lists/{list_new.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        """тест: ошибки валидации отсылаются назад в шаблон домашней страницы"""
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_arent_saved(self):
+        """тест: сохраняются недопустимые элементы списка"""
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
 
 
 class NewItemTest(TestCase):
