d413fc8d9f231e6f2cf67b392710f97a79fb96b7
Vel <valera-shih@mail.ru>
2023-02-26 20:37:03 +0400

    Добавлен ФТ регистрации, добавлена форма в base.html

--- superlists/settings.py
+++ superlists/settings.py
@@ -119,3 +119,20 @@ STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'console': {
+        'handlers': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
