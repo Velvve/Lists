049df9fb5eeffbff79d6803c37f268f23b6472f4
Vel <valera-shih@mail.ru>
2023-03-21 18:54:03 +0400

    Рефакторинг

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,19 @@ import time
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     """Функциональный тест"""
 
@@ -21,6 +34,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         """демонтаж"""
         self.browser.quit()
 
+    @wait
     def wait_for_row_in_list_table(self, row_text):
         """Ожидание строки в таблице списка"""
         start_time = time.time()
@@ -35,21 +49,16 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
+    @wait
     def wait_for(self, fn):
         """ожидание"""
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         """получить поле ввода для элемента"""
         return self.browser.find_element('id', 'id_text')
 
+    @wait
     def wait_to_be_logged_in(self, email):
         """ожидание входа в систему"""
         self.wait_for(
@@ -58,6 +67,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         navbar = self.browser.find_element('css selector', '.navbar')
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         """ожидать выхода из системы"""
         self.wait_for(
